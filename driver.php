be8851d46cc51defbeb52ac426ee4d013157f7e4
Lavanya Goyal <87686764+Lavanya11G@users.noreply.github.com>
2023-02-16 00:25:41 +0530

    Create driver.php

---
+++ driver.php
@@ -0,0 +1,273 @@
+<?php
+ 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ashok_leyland";
+ 
+// Connection
+$conn = mysqli_connect($servername,
+               $username, $password, $dbname);
+ 
+// Check if connection is
+// Successful or not
+if (!$conn) {
+  die("Connection failed: "
+      . mysqli_connect_error());
+}
+$id=$_POST['fq_name'];
+$sql="SELECT * FROM ashok_leyland WHERE driver_id='$id'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>DEVELOPERS x LEYLAND</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <div class="site-wrap">
+
+        <div class="site-mobile-menu">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+
+        <header style="backgroud-color:rgb(0, 0, 0)" class="site-navbar py-3" role="banner" style="background-color:white;opacity:0.4">
+
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <div class="col-11 col-xl-2">
+                        <img src="/logistics-master/images/Logo.png" style="width: 300px;height:70px">
+                    </div>
+                    <div class="col-12 col-md-10 d-none d-xl-block">
+                        <nav class="site-navigation position-relative text-right" role="navigation">
+
+                            <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                                <li class="active"><a href="index.html">Home</a></li>
+                                <li><a href="about.html" style="color:rgb(0, 0, 0)">Track Driver</a></li>
+                                <li><a href="about.html" style="color:rgb(0, 0, 0)">Contact Driver</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+        </header>
+
+
+
+        
+
+        <div class="site-section">
+            <div class="container">
+                <div class="row justify-content-center mb-5">
+                    <div class="col-md-7 text-center border-primary">
+                        <h2 class="mb-0 text-primary">Driver's Consinment </h2>
+
+                    </div>
+                </div>
+
+                <div class="site-section bg-light">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 mb-5">
+
+
+
+                                <form action="#" class="p-5 bg-white">
+
+
+                                    <div class="row form-group">
+                                        <div class="col-md-6 mb-3 mb-md-0">
+                                            <label class="text-black" for="fname">Driver's ID :</label><?php echo "  ".$row["driver_id"] ?>
+
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="lname">GPS Provider :</label><?php echo "  ".$row["GpsProvider"] ?>
+                                        </div>
+                                    </div>
+
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="email">Booking ID:</label><?php echo "  ".$row["BookingID"] ?>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Booking Date:</label><?php echo "  ".$row["BookingID_Date"] ?>
+
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Vehicle Number:</label><?php echo "  ".$row["vehicle_no"] ?>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Pickup Location :</label><?php echo "  ".$row["Origin_Location"] ?>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Destination Location :</label><?php echo "  ".$row["Destination_Location"] ?>
+
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Planned ETA :</label><?php echo "  ".$row["Planned_ETA"] ?>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Current Location :</label><?php echo "  ".$row["Current_Location"] ?>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Trip Start Date:</label><?php echo "  ".$row["trip_start_date"] ?>
+
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+
+                                        <div class="col-md-6">
+                                            <label class="text-black" for="subject">Customer Name Code:</label><?php echo "  ".$row["customerNameCode"] ?>
+
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h2 class="footer-heading mb-4">Quick Links</h2>
+                                <ul class="list-unstyled">
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Track Drivers</a></li>
+                                    <li><a href="#">Contact Drivers</a></li>
+
+                                </ul>
+                            </div>
+                            <div class="col-md-4">
+                                <h2 class="footer-heading mb-4">Products</h2>
+                                <ul class="list-unstyled">
+                                    <li><a href="https://www.ashokleyland.com/in/en/products/trucks?tab=2">ICV Distributions</a></li>
+                                    <li><a href="https://www.ashokleyland.com/in/en/products/trucks?tab=1">Haulage</a></li>
+                                    <li><a href="https://www.ashokleyland.com/in/en/products/trucks?tab=4">Tractors</a></li>
+                                    <li><a href="https://www.ashokleyland.com/in/en/products/trucks?tab=3">Tippers</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h2 class="footer-heading mb-4">Follow Us</h2>
+                                <a href="https://www.facebook.com/AshokLeylandOfficial/" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                                <a href="https://twitter.com/ALIndiaOfficial" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                                <a href="https://www.instagram.com/ashokleyland_official/?hl=en" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                                <a href="https://www.linkedin.com/company/ashok-leyland/" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row pt-5 mt-5 text-center">
+                    <div class="col-md-12">
+                        <div class="border-top pt-5">
+                            <p>
+
+                                Copyright &copy;
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved <i class="icon-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Dot.Developers</a>
+
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
